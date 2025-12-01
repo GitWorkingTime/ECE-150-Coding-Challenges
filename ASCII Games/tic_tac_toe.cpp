97fe3a4cabe56284f73b76e6e7c32e27052cb738
GitWorkingTime <dlee28262@gmail.com>
2025-12-01 10:27:40 -0500

    Finished Tic Tac Toe!

--- ASCII Games/tic_tac_toe.cpp
+++ ASCII Games/tic_tac_toe.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 
 int main();
-void DisplayTable( char choices[3][3] );
+void displayTable( char choices[3][3] );
 void playersTurn( char choices[3][3] );
 void computersTurn( char choices[3][3] );
+unsigned int checkWinner( char choices[3][3], char player );
 
 int main() {
     char choices[3][3]{
@@ -15,8 +18,9 @@ int main() {
     char first{};
     unsigned short column{0};
     unsigned short row{0};
+    unsigned short turns{0};
 
-    DisplayTable( choices );
+    displayTable( choices );
     
     do {
         std::cout << "Do you want to go first? (y\\n): ";
@@ -27,17 +31,69 @@ int main() {
 
     } while ( !(first == 'y' || first == 'Y') && !(first == 'n' || first == 'N') );
 
-    if( first == 'y' || first == 'Y' ) {
-        playersTurn( choices );
-    } else {
+    if(( first == 'y' ) || ( first == 'Y' )) {
+        do {
+            ++turns;
+            playersTurn( choices );
 
+            if( turns >= 3 ) {
+                unsigned short status = checkWinner( choices,  'X' );
+                if(status == 1) {
+                    std::cout << "YOU WON!" << std::endl;
+                    break;
+                }
+            }
+
+            ++turns;
+            computersTurn( choices );
+
+            if( turns >= 3 ) {
+                unsigned short status = checkWinner( choices,  'O' );
+                if( status == 2) {
+                    std::cout << "YOU LOST!" << std::endl;
+                    break;
+                }
+            }
+        } while ( turns < 10 );
+
+        if( (checkWinner( choices, 'X' ) == 0 ) && ( checkWinner( choices, 'O' ) == 0 ) ){
+            std::cout << "DRAW!" << std::endl;
+        }
+    } else {
+        do {
+            ++turns;
+            computersTurn( choices );
+
+            if( turns >= 3 ) {
+                unsigned short status = checkWinner( choices,  'O' );
+                if( status == 2) {
+                    std::cout << "YOU LOST!" << std::endl;
+                    break;
+                }
+            }
+
+            ++turns;
+            playersTurn( choices );
+
+            if( turns >= 3 ) {
+                unsigned short status = checkWinner( choices,  'X' );
+                if(status == 1) {
+                    std::cout << "YOU WON!" << std::endl;
+                    break;
+                }
+            }
+        } while ( turns < 10 );
+
+        if( (checkWinner( choices, 'X' ) == 0 ) && ( checkWinner( choices, 'O' ) == 0 ) ){
+            std::cout << "DRAW!" << std::endl;
+        }
     }
 
 
     return 0;
 }
 
-void DisplayTable( char choices[3][3] ) {
+void displayTable( char choices[3][3] ) {
     std::cout << std::endl;
     std::cout << "    1   2   3 " << std::endl;
     std::cout << "      |   |   " << std::endl;
@@ -92,11 +148,70 @@ void playersTurn( char choices[3][3] ) {
         choices[column][row] = 'X';
     }
 
-    DisplayTable(choices);
+    displayTable(choices);
     return;
 }
 
 void computersTurn( char choices[3][3] ) {
+    srand(time(NULL));
+    unsigned short row{0};
+    unsigned short column{0};
+
+    do {
+        row = (unsigned short)rand() % 3;
+        column = (unsigned short)rand() % 3;
+
+    } while( choices[row][column] != ' ');
+
+    choices[row][column] = 'O';
+    displayTable(choices);
 
     return;
+}
+
+unsigned int checkWinner( char choices[3][3], char player ) {
+    unsigned short status{0};
+
+    if( player == 'X' ) {
+        status = 1;
+        if( (choices[0][0] == player) && (choices[0][1] == player) && (choices[0][2] == player) ) {
+            return status;
+        } else if( (choices[1][0] == player) && (choices[1][1] == player) && (choices[1][2] == player) ) {
+            return status;
+        } else if ( (choices[2][0] == player) && (choices[2][1] == player) && (choices[2][2] == player) ) {
+            return status;
+        } else if ( (choices[0][0] == player) && (choices[1][1] == player)  && (choices[2][2] == player) ) {
+            return status;            
+        } else if ( (choices[0][2] == player) && (choices[1][1] == player) && (choices[2][0] == player) ) {
+            return status;         
+        } else if ( (choices[0][0] == player) && (choices[1][0] == player) && (choices[2][0] == player) ) {
+            return status;            
+        } else if ( (choices[0][1] == player) && (choices[1][1] == player) && (choices[2][1] == player) ) {
+            return status;            
+        } else if ( (choices[0][2] == player) && (choices[1][2] == player) && (choices[2][2] == player) ) {
+            return status;            
+        }
+    } else {
+        status = 2;
+        if( (choices[0][0] == player) && (choices[0][1] == player) && (choices[0][2] == player) ) {
+            return status;
+        } else if( (choices[1][0] == player) && (choices[1][1] == player) && (choices[1][2] == player) ) {
+            return status;
+        } else if ( (choices[2][0] == player) && (choices[2][1] == player) && (choices[2][2] == player) ) {
+            return status;
+        } else if ( (choices[0][0] == player) && (choices[1][1] == player)  && (choices[2][2] == player) ) {
+            return status;            
+        } else if ( (choices[0][2] == player) && (choices[1][1] == player) && (choices[2][0] == player) ) {
+            return status;         
+        } else if ( (choices[0][0] == player) && (choices[1][0] == player) && (choices[2][0] == player) ) {
+            return status;            
+        } else if ( (choices[0][1] == player) && (choices[1][1] == player) && (choices[2][1] == player) ) {
+            return status;            
+        } else if ( (choices[0][2] == player) && (choices[1][2] == player) && (choices[2][2] == player) ) {
+            return status;            
+        }
+    }
+
+    status = 0;
+    return status;
 }
